5b865412e70c66acff3b56eee9a8f4d8c4bcb087
oriolsubirana <subi.cat@gmail.com>
2019-01-27 18:56:40 +0100

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return <div className="ui container" style={{marginTop: '10px'}}><SearchBar></SearchBar></div>
+class App extends React.Component {
+    onSearchSubmit(term) {
+        axios.get('https://api.unsplash.com/search/photos', {
+        params: { query: term },    
+        headers: {
+                Authorization: 
+                'Client-ID bbf9f2c61e68c21e20036689ace35275a3198e5514291b037b79cfec5750f0f2'
+            }
+        });    
+    }
+    render() {
+        return (
+            <div className="ui container" style={{marginTop: '10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit} />            
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,25 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    onInputChange(event) {
-        event.target.value
+    state = { term: '' };
+
+    //para que no refresque la pagina al dar intro
+    onFormSubmit = event => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.term);
     }
-    render() {
+
+     render() {
         return (
-            <div class="ui segment">
-                <form className="ui form">
+            <div className="ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" onChange={this.onInputChange}></input>
+                        <input 
+                            type="text" 
+                            value={this.state.term}
+                            onChange={e => this.setState({term : this.onInputChange})}
+                        ></input>
                     </div>
                 </form>
             </div>
